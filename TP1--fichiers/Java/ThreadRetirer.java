9d1e08edc97f78dd87597adf89e355b74adee567
l3miage-barkoko <omar.barkok@etu.univ-grenoble-alpes.fr>
2026-09-18 19:29:09 +0200

    add: update gitignore + readme (until 2.5)

--- TP1--fichiers/Java/ThreadRetirer.java
+++ TP1--fichiers/Java/ThreadRetirer.java
@@ -1,7 +1,7 @@
 /**
  * Thread qui retire
  */
-class ThreadRetirer extends Thread {
+class ThreadRetirer implements Runnable {
 
     private int number;
     private Tableau t;
@@ -13,6 +13,7 @@ class ThreadRetirer extends Thread {
         this.nbIter = nbIterations;
     }
 
+    @Override
     public void run() {
         for (int j = 0; j < nbIter; j++)
             t.decTab();
