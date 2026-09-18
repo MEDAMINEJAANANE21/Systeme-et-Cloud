ccdb0da53c79ddfeeac54581b26f29df59c6f7bc
jaananem <Mohamed-amine.jaanane@etu.univ-grenoble-alpes.fr>
2026-09-18 17:15:17 +0200

    Add: Commentaire

--- TP1--fichiers/Java/ExempleThread2.java
+++ TP1--fichiers/Java/ExempleThread2.java
@@ -12,11 +12,13 @@ public class ExempleThread2 extends Thread {
     }
 
     public void run() {
+   
         try {
             TimeUnit.MILLISECONDS.sleep(toWait);
         } catch (InterruptedException e) {
             UnexpectedSituation.exit("interrupted sleep in thread "+ Thread.currentThread().getName(), e);
         }
+        
         System.out.print(toSay);
     }
 
@@ -24,8 +26,8 @@ public class ExempleThread2 extends Thread {
         ExempleThread2 thread1, thread2, thread3;
 
         thread1=new ExempleThread2(1, "Hello ", 50);
-        thread2=new ExempleThread2(2, "World ",25);
-        thread3=new ExempleThread2(3, "and Everybody ",40);
+        thread2=new ExempleThread2(2, "World ",40);
+        thread3=new ExempleThread2(3, "and Everybody ",1);
 
         thread1.start();
         thread2.start();
